a56d899e948464a4b1659b383a3ef51152a4c6b7
jannik1020 <jannik.thiele.official@gmail.com>
2026-01-13 16:40:22 +0100

    onServerTick takes different spawn intervals of SpawnerTiers into account

--- src/main/java/de/thieles/bedwarsplugin/listeners/SpawnerTickListener.java
+++ src/main/java/de/thieles/bedwarsplugin/listeners/SpawnerTickListener.java
@@ -9,26 +9,27 @@ import org.bukkit.event.Listener;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.util.Vector;
 
+import java.util.Arrays;
+
 public class SpawnerTickListener implements Listener {
     private final BedwarsSetup setup;
 
     public SpawnerTickListener(BedwarsSetup setup) {
+
         this.setup = setup;
     }
 
     @EventHandler
     public void onServerTick(ServerTickStartEvent event) {
-        if(event.getTickNumber() % 100 == 0) {
-            setup.getSpawnerBlocks().forEach(spawnerBlock -> {
-                        Item item = spawnerBlock.block().getWorld().dropItem(
-                                spawnerBlock.block().getLocation(),
-                                new ItemStack(spawnerBlock.tier().getItem())
-                        );
-                        item.setVelocity(new Vector(0, 0, 0));
-                        item.setGravity(false);
-
-                    }
-            );
-        }
+        setup.getSpawnerBlocks().forEach(spawnerBlock -> {
+            if(event.getTickNumber() % spawnerBlock.tier().getTicksBetweenSpawn() == 0) {
+                Item item = spawnerBlock.block().getWorld().dropItem(
+                        spawnerBlock.block().getLocation().add(0,1,0),
+                        new ItemStack(spawnerBlock.tier().getItem())
+                );
+                item.setVelocity(new Vector(0, 0, 0));
+                item.setGravity(true);
+            }
+        });
     }
 }
