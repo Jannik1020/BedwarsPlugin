ca547150cc03a7de6b144836d2cf646cbf313766
jannik1020 <jannik.thiele.official@gmail.com>
2026-01-14 16:09:40 +0100

    implemented feature to load saved beds and spawners on startup

--- src/main/java/de/thieles/bedwarsplugin/BedwarsPlugin.java
+++ src/main/java/de/thieles/bedwarsplugin/BedwarsPlugin.java
@@ -6,14 +6,15 @@ import de.thieles.bedwarsplugin.listeners.PlayerJoinListener;
 import de.thieles.bedwarsplugin.listeners.SpawnerTickListener;
 import org.bukkit.Bukkit;
 import org.bukkit.Location;
+import org.bukkit.World;
 import org.bukkit.block.Block;
+import org.bukkit.configuration.ConfigurationSection;
 import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.util.Vector;
+import org.jetbrains.annotations.NotNull;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 public final class BedwarsPlugin extends JavaPlugin {
     BedwarsGame game = new BedwarsGame();
@@ -29,6 +30,8 @@ public final class BedwarsPlugin extends JavaPlugin {
         // Commands
         BedwarsAdminCommand bwaCmd = new BedwarsAdminCommand(game, this);
         registerCommand(BedwarsAdminCommand.CMD_NAME, bwaCmd);
+
+        loadBedwarsConfig();
     }
 
     @Override
@@ -37,21 +40,63 @@ public final class BedwarsPlugin extends JavaPlugin {
         saveBedwarsConfig();
     }
 
-    private void loadConfig(BedwarsSetup setup) {
-        FileConfiguration cfg = getConfig();
+    public void loadBedwarsConfig() {
+        loadBeds();
+        loadSpawnerBlocks();
     }
 
+
+
+    private void loadBeds() {
+        FileConfiguration cfg = getConfig();
+        ConfigurationSection beds = cfg.getConfigurationSection("beds");
+
+        if(beds == null) {
+            getLogger().info("Could not load config section 'beds'");
+            return;
+        }
+
+        Set<String> teamIdents = beds.getKeys(false);
+        for(String s: teamIdents) {
+            ConfigurationSection team = beds.getConfigurationSection(s);
+            if (team == null) continue;
+
+            String worldName = team.getString("world");
+            World world = Bukkit.getWorld(worldName);
+
+            if (world == null) {
+                getLogger().warning("World not found for team " + team);
+                continue;
+            }
+
+            int fx = team.getInt("foot.x");
+            int fy = team.getInt("foot.y");
+            int fz = team.getInt("foot.z");
+
+            int hx = team.getInt("head.x");
+            int hy = team.getInt("head.y");
+            int hz = team.getInt("head.z");
+
+            Block foot = world.getBlockAt(fx, fy, fz);
+            Block head = world.getBlockAt(hx, hy, hz);
+
+            BedwarsSetup.TeamBed bed = new BedwarsSetup.TeamBed(head, foot, s);
+            game.getSetup().addTeamBed(bed);
+        }
+    }
     private void saveBed(Block bedFoot, Block bedHead, FileConfiguration cfg, String path) {
         Location locFoot = bedFoot.getLocation();
         Location locHead = bedHead.getLocation();
 
         cfg.set(path + ".world", locFoot.getWorld().getName());
-        cfg.set(path + ".footX", locFoot.getBlockX());
-        cfg.set(path + ".footY", locFoot.getBlockY());
-        cfg.set(path + ".footZ", locFoot.getBlockZ());
-        cfg.set(path + ".headX", locHead.getBlockX());
-        cfg.set(path + ".headZ", locHead.getBlockZ());
 
+        cfg.set(path + ".foot.x", locFoot.getBlockX());
+        cfg.set(path + ".foot.y", locFoot.getBlockY());
+        cfg.set(path + ".foot.z", locFoot.getBlockZ());
+
+        cfg.set(path + ".head.x", locHead.getBlockX());
+        cfg.set(path + ".head.y", locHead.getBlockY());
+        cfg.set(path + ".head.z", locHead.getBlockZ());
         saveConfig();
     }
 
@@ -60,7 +105,33 @@ public final class BedwarsPlugin extends JavaPlugin {
             saveBed(bed.foot(), bed.head(), cfg, "beds." + bed.teamIdent());
         });
     }
+    private void loadSpawnerBlocks() {
+        FileConfiguration cfg = getConfig();
+        @NotNull List<Map<?, ?>> spawners = cfg.getMapList("spawners");
+        for(Map<?,?> spawnerBlock: spawners){
+            String worldName = (String) spawnerBlock.get("world");
+            int x = (int) spawnerBlock.get("x");
+            int y = (int) spawnerBlock.get("y");
+            int z = (int) spawnerBlock.get("z");
+            String tierTag = (String) spawnerBlock.get("tier");
 
+            World world = Bukkit.getWorld(worldName);
+            if (world == null) {
+                getLogger().warning("World not found: " + worldName);
+                continue;
+            }
+
+            Optional<BedwarsSetup.SpawnerTier> tier = BedwarsSetup.SpawnerTier.fromTag(tierTag);
+            if(tier.isEmpty()) {
+                getLogger().warning("Invalid tier at " + worldName + " " + x + " " + y + " " + z);
+                continue;
+            }
+
+            Location location = new Location(world, x, y, z);
+            BedwarsSetup.SpawnerBlock spawner = new BedwarsSetup.SpawnerBlock(location.getBlock(), tier.get());
+            game.getSetup().addSpawnerBlock(spawner);
+        }
+    }
     private void saveSpawnerBlocks(List<BedwarsSetup.SpawnerBlock> blocks, FileConfiguration cfg) {
         String path = "spawners";
         List<Map<String, Object>> spawnerList = new ArrayList<>();
@@ -75,6 +146,7 @@ public final class BedwarsPlugin extends JavaPlugin {
             data.put("tier", spawner.tier().getTagName());
             spawnerList.add(data);
         }
+
 
         cfg.set(path, spawnerList);
         saveConfig();
